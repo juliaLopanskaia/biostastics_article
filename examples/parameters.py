4f5d5108e94d0708e863b6bf8b5f0bd19b0b9014
Julia Lopanskaia <roxu9600@bk.ru>
2021-03-08 22:09:20 +0300

    добавлена папка, из которой будет запускаться скрипт

---
+++ examples/parameters.py
@@ -0,0 +1,8 @@
+N_per_cluster = 10; # number of experiments per cluster
+N_clusters = 3; # number of clusters
+
+true_exp_mean = 0.8; # a true value of experimental measurement (e.g. measurement with drug)
+true_control_mean = 0.8; # a true value of control measurement (e.g. measurement without drug)
+
+inter_day_SD = 0.25; # the inter day data varience
+intra_day_SD = 0.3; # the intraday data varience
