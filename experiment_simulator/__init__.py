def5e50a29bce83d501970d9665f54ba43049c6b
Julia Lopanskaia <roxu9600@bk.ru>
2021-03-09 15:51:22 +0300

    changed init.py

--- experiment_simulator/__init__.py
+++ experiment_simulator/__init__.py
@@ -1,1 +1,5 @@
-__all__ = ('generate_data', 'figure_generate', 'ttest_print', 'correct_p_value')
+#__all__ = ['generate_data', 'ttest_print', 'figure_generate', 'correct_p_value']
+from experiment_simulator.generate_data import generate_data
+from experiment_simulator.figure_generate import figure_generate
+from experiment_simulator.ttest_print import ttest_print
+from experiment_simulator.correct_p_value import correct_p_value
