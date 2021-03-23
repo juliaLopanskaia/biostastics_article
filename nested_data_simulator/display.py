8ae47abef01344c5dde8f2581d79765c6e89aba4
Julia Lopanskaia <roxu9600@bk.ru>
2021-03-23 13:27:18 +0300

    gray -> white bg in display.py

--- nested_data_simulator/display.py
+++ nested_data_simulator/display.py
@@ -4,33 +4,42 @@ import matplotlib.pyplot as plt
 import seaborn as sns; sns.set_theme()
 import matplotlib.ticker as ticker
 
+
 def display_data(data_exp, data_control, N_clusters:int, N_per_cluster:int):
     """ display data (all experiments and means per clusters)
-
     INPUT: experimental data (matrix) & control data (matrix)
            the number of clusters and the number of experiments per clusters
-
     OUTPUT: None """
-    #ipdb.set_trace()
     data_exp_mean = data_exp.mean(axis=0)
     data_control_mean = data_control.mean(axis=0)
+    plt.rcParams["axes.edgecolor"] = "black"
+    plt.rcParams["axes.linewidth"] = 1
     fig, ax = plt.subplots()
     fig = plt.plot(np.ones((N_per_cluster,1))+0.05/np.sqrt(N_clusters)*np.random.randn(N_per_cluster,1), data_exp,'.',markersize=6)
-
+    # colors
     colord = []
     for i in range(len(fig)):
         colord.append(fig[i].get_color())
     col=colord
-
     plt.scatter(np.ones(N_clusters), data_exp_mean, 1000, col,'+',lineWidths=3)
-
     arr_control=2*np.ones((N_per_cluster,1))+0.05/np.sqrt(N_clusters)*np.random.randn(N_per_cluster,1)
     for i in range(N_per_cluster):
         for j in range(N_clusters):
             plt.plot(arr_control[i], data_control[i][j],'.',markersize=6,color=col[j])
     plt.scatter(2*np.ones(N_clusters), data_control_mean, 1000, col,'+',lineWidths=3)
-
+    # axis
     ax.set_xlim(0,3)
+    ax.patch.set_visible(False)
+    plt.xticks([])
+    plt.xlabel('exp                      control')
+    #  axis intervals
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(0.2))
+    ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.1))
+    ax.patch.set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
     plt.show()
 
 
